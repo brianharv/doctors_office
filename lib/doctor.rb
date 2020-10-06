8d81106267b7dfc9c8a896cd01ba3bccfa365e56
Brian Harvey <brian.harv3y@gmail.com>
2020-10-06 12:01:30 -0700

    begin building logic for doctors and patients
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com>

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,25 @@
-#doctor will have name id attributes
+class Doctor
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def ==(doctor_to_compare)
+    (self.name() == doctor_to_compare.name()) && (self.id() == doctor_to_compare.id())
+  end
+  
+  def self.all
+    returned_doctors = DB.exec("SELECT * FROM doctors;")
+    doctors = []
+    returned_doctors.each() do |doctor|
+      name = attributes.fetch("name")
+      id = attributes.fetch("id").to_i
+      doctors.push(Doctor.new({:name => name, :id => id}))
+    end
+    doctors
+  end   
+  
+  
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
 # patient will have name, birthdate, doctor id
+class Patient
+  attr_reader: :id 
+  attr_accessor: :name , :doctor_id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+    @doctor_id = attributes.fetch(:id)
+  end
+
+  def ==(patient_to_compare)
+    (self.name() == patient_to_compare.name()) && (self.album_id() == patient_to_compare.album_id())
+  end
+
+  def self.all
+    returned_patients = DB.exec("SELECT * FROM patients;")
+    patients = []
+    returned_patients.each() do |patients|
+      name = patients.fetch("name")
+      album_id = patients.fetch("album_id").to_i
+      id = patients.fetch("id").to_i
+      patients.push(Song.new({:name => name, :album_id => album_id, :id => id}))
+    end
+    patients
+  end
+
+
+
