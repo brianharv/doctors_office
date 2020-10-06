ad8d0f9bc2f1891f18dd697cacca0b67fafa4e6d
Brian Harvey <brian.harv3y@gmail.com>
2020-10-06 16:27:50 -0700

    build out app.rb and add views
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com>

--- app.rb
+++ app.rb
@@ -13,17 +13,67 @@ get('/') do
 end
 
 
-# get for /doctors
-get('/docto')
-# get for /doctors/:id
-# post for /doctors
-# patch for /doctors/:id
-# delete for /doctors/:id
+get('/doctors') do
+  @doctors = Doctor.all 
+  erb(:doctors)
+end
 
-# get for /doctors/:id/patients/:patient_id
+get('/doctors/:id')
+  @doctors = Doctor.find(params[:id].to_i)
+  erb(:doctor)
+end
+
+get('/doctors/new') do
+  erb(:new_doctor)
+end
+
+post('/doctors') do
+  name = params[:name]
+  doctor = Doctor.new({:name => name, :id => nil})
+  doctor.save()
+  redirect to('/doctors')
+end
+
+patch('/doctors/:id') do
+  @doctor = Doctor.find(params[:id].to_i)
+  @doctor.update(param[:name])
+  redirect to('/doctors')
+end
+
+delete('/doctors/:id') do
+  @doctor = Doctor.find(params[:id].to_i)
+  @doctor.delete()
+  redirect to('/doctors')
+end
+  
+### PATIENTS ###
+
+get('/doctors/:id/patients/:patient_id') do
+  @patient = Patient.find(params[:patient_id].to_i())
+  erb(:song)
+end
+
 # post for /doctors/:id/patients
+post ('/doctors/:id/patients') do
+  @doctor = Doctor.find(params[:id].to_i())
+  patient = Patient.new({:name => params[:patient_name], :doctor_id => @doctor.id, :id => nil})
+  patient.save()
+  erb(:doctor)
+end
 # patch for /doctors/:id/patients/:patient_id
+patch ('/doctors/:id/patients/:patient_id') do
+  @doctor = Doctor.find(params[:id].to_i())
+  patient = Patient.find(params[:patient_id].to_i())
+  patient.update(params[:name], @doctor.id)
+  erb(:doctor)
+end
 # delete for /doctors/:id/patients/:patient_id
+delete ('/doctors/:id/patients/:patient_id') do
+  patient = Patient.find(params[:patient_id].to_i())
+  patient.delete
+  @doctor = Doctor.find(params[:id].to_i())
+  erb(:doctor)
+end
 
 
 # ? get /doctors/new -- takes us to the form for adding a doctor??
